78e3d6a8c16b17fa256c69963ba56530f1319256
EUNHYE HAN <hellou8363@gmail.com>
2023-04-06 12:58:16 +0900

    refactor : ReviewService Interface 삭제

--- src/main/java/org/zerock/wego/service/ReviewService.java
+++ src/main/java/org/zerock/wego/service/ReviewService.java
@@ -17,56 +17,40 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RequiredArgsConstructor
 
-<<<<<<< HEAD
 @Service
 public class ReviewService {
-=======
-	// 특정 후기글 상세조회(READ)
-	public abstract ReviewViewVO getById(Integer reviewId) throws ServiceException;
->>>>>>> branch 'main' of https://github.com/hellou8363/project-wego.git
-
-<<<<<<< HEAD
-	private final ReviewMapper mapper;
-=======
-	// 특정 후기글 삭제(DELETE)
-	public abstract boolean isRemoved(Integer reviewId) throws ServiceException;
->>>>>>> branch 'main' of https://github.com/hellou8363/project-wego.git
-
-<<<<<<< HEAD
+	
+	private final ReviewMapper reviewMapper;
+	
+	
 	public List<ReviewViewVO> getList() throws ServiceException {
 		log.trace("getList() invoked.");
 		
 		try {
-			return this.mapper.selectAll();
+			return this.reviewMapper.selectAll();
 		} catch (Exception e) {
 			throw new ServiceException(e);
 		} // try-catch
 	} // getList
-=======
-	// 새로운 게시물 등록(CREATE)
-	public abstract boolean isRegistered(ReviewDTO dto) throws ServiceException;
->>>>>>> branch 'main' of https://github.com/hellou8363/project-wego.git
-
-<<<<<<< HEAD
+	
+	
 	public Set<ReviewViewVO> getRandom10List() throws ServiceException {
 		log.trace("getRandom10List() invoked.");
 		
 		try {
-			return this.mapper.selectRandom10();
+			return this.reviewMapper.selectRandom10();
 		} catch (Exception e) {
 			throw new ServiceException(e);
 		} // try-catch
 	} // getList
-=======
-	// 기존 게시물 수정(UPDATE)
-	public abstract boolean isModified(ReviewDTO dto) throws ServiceException;
->>>>>>> branch 'main' of https://github.com/hellou8363/project-wego.git
-
-	public ReviewViewVO getReviewByReviewId(Integer sanReviewId) throws ServiceException {
-		log.trace("get({}) invoked.", sanReviewId);	
+	
+	
+	// 특정 후기글 조회 
+	public ReviewViewVO getById(Integer reviewId) throws ServiceException {
+		log.trace("getById({}) invoked.", reviewId);	
 		
 		try {
-			ReviewViewVO review = this.mapper.selectReviewByReviewId(sanReviewId);
+			ReviewViewVO review = this.reviewMapper.selectById(reviewId);
 			Objects.requireNonNull(review);
 
 			
@@ -75,34 +59,36 @@ public class ReviewService {
 			throw new ServiceException(e);
 		} // try-catch
 	} // get
-
-	public boolean isReviewRemove(Integer sanReviewId) throws ServiceException {
-		log.trace("remove({}) invoked.", sanReviewId);
+	
+	
+	// 특정 후기글 삭제
+	public boolean isRemoved(Integer reviewId) throws ServiceException {
+		log.trace("isRemoved({}) invoked.", reviewId);
 		
 		try {
-			
-			return this.mapper.deleteReviewByReviewId(sanReviewId) == 1;
-			
+			return this.reviewMapper.delete(reviewId) == 1;
 		} catch (Exception e) {
 			throw new ServiceException(e);
 		} // try-catch
 	} // remove
-
-	public boolean register(ReviewDTO dto) throws ServiceException {
+	
+	
+	public boolean isRegistered(ReviewDTO dto) throws ServiceException {
 		log.trace("register({}) invoked.");
 		
 		try {
-			return this.mapper.insert(dto) == 1;
+			return this.reviewMapper.insert(dto) == 1;
 		} catch (Exception e) {
 			throw new ServiceException(e);
 		} // try-catch
 	} // register
-
-	public boolean modify(ReviewDTO dto) throws ServiceException {
+	
+	
+	public boolean isModified(ReviewDTO dto) throws ServiceException {
 		log.trace("modify({}) invoked.");
 		
 		try {
-			return this.mapper.update(dto) == 1;
+			return this.reviewMapper.update(dto) == 1;
 		} catch (Exception e) {
 			throw new ServiceException(e);
 		} // try-catch
