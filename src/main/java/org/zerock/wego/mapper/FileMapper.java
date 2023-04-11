197028a0035d8227b99f122b27337357d24b6534
EUNHYE HAN <hellou8363@gmail.com>
2023-04-11 15:13:06 +0900

    feat, refactor: 모집, 후기, 로그인 스타일 수정 / 좋아요 조건문 추가 / FileService 기능추가

--- src/main/java/org/zerock/wego/mapper/FileMapper.java
+++ src/main/java/org/zerock/wego/mapper/FileMapper.java
@@ -1,12 +1,9 @@
 package org.zerock.wego.mapper;
 
 import java.util.List;
-import java.util.Set;
 
-import org.apache.ibatis.annotations.Select;
 import org.zerock.wego.domain.FileDTO;
 import org.zerock.wego.domain.FileVO;
-import org.zerock.wego.domain.SanInfoViewVO;
 
 
 public interface FileMapper {
@@ -14,6 +11,7 @@ public interface FileMapper {
 	public abstract List<FileVO> select(String targetGb, Integer targetCd); // 파일 조회
 	public abstract Integer insert(FileDTO file); // 신규 파일 등록
 	public abstract Integer update(String targetGb, Integer targetCd, Integer fileId, String fileName); // 기존 파일 수정
-	public abstract Integer delete(String targetGb, Integer targetCd); // 파일 삭제
+	public abstract Integer deleteAll(String targetGb, Integer targetCd); // 파일 삭제
+	public abstract Integer delete(String targetGb, Integer targetCd, String uuid); // 특정 파일 삭제
 	
 } // end interface
