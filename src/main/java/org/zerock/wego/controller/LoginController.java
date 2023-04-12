a1fdf9573018790c9d65d68136e9b212ce4378ff
javahasooingu <ingoo1024@gmail.com>
2023-04-12 11:49:45 +0900

    update login

--- src/main/java/org/zerock/wego/controller/LoginController.java
+++ src/main/java/org/zerock/wego/controller/LoginController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+import org.zerock.wego.config.SessionConfig;
 import org.zerock.wego.domain.UserDTO;
 import org.zerock.wego.domain.UserVO;
 import org.zerock.wego.exception.ControllerException;
@@ -20,37 +22,21 @@ import org.zerock.wego.oauth.KakaoOAuth;
 import org.zerock.wego.service.LoginService;
 import org.zerock.wego.service.OAuthService;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RequiredArgsConstructor
 
-@SessionAttributes({"__AUTH__"})
 @RequestMapping("/login")
 @Controller
-public class LoginController 
-implements InitializingBean {
-
+public class LoginController {
 
 	private final KakaoOAuth kakaoOAuth;
-	private final LoginService loginService;
 	private final OAuthService oAuthService;
 
-	@Override
-	public void afterPropertiesSet() throws ControllerException {
-		log.trace("afterPropertiesSet() invoked");
-
-		try {
-			Objects.requireNonNull(this.kakaoOAuth);
-			Objects.requireNonNull(this.loginService);
-			Objects.requireNonNull(this.oAuthService);
-
-		} 
-		catch (Exception e) {
-			throw new ControllerException(e);
-		} // try-catch
-	} // afterPropertiesSet
 
 	@GetMapping
 	public String showLogin() throws ControllerException{
@@ -64,64 +50,21 @@ implements InitializingBean {
 	public String getKakaoLogin() throws ControllerException{
 		log.trace("getKakaoLogin() invoked.");
 
-		try {
-			StringBuffer redirectString = new StringBuffer("redirect:");
-
-			redirectString.append(kakaoOAuth.getLoginURLToGetAuthorizationCode());
-
-			return redirectString.toString();
-		} catch (Exception e) {
-			throw new ControllerException(e);
-		}// try-catch
-		
+		return "redirect:" + kakaoOAuth.getLoginURLToGetAuthorizationCode();
 	}// getKakaoLogin
 
 
 	@GetMapping("/kakao/oauth")
-	public String getKakaoCallBack(
-			@RequestParam("code") String authorizationCode,
-			Model model
-			) throws ControllerException{
-		log.trace("getAccessToken({}, model) invoked.", authorizationCode);
+	public String getKakaoCallBack(@RequestParam("code") String authorizationCode, Model model) throws JsonProcessingException {
+		log.trace("getAccessToken({}, ModelAndView) invoked.", authorizationCode);
 
-		try {
-			UserDTO kakaoUserDTO = oAuthService.kakaoLogin(authorizationCode);
-			// 수정 유저 인포 갖고와서 그걸로 다시 회원가입 되어 있는지 로그인 서비스에 전달하자
+		UserVO kakaoUserVO = oAuthService.kakaoLogin(authorizationCode);
 
-			log.info("userDTO = {}", kakaoUserDTO);
+		model.addAttribute(SessionConfig.AUTH_KEY_NAME, kakaoUserVO);
 
-			UserVO kakaoOAuth = this.loginService.login(kakaoUserDTO);
-
-			if(kakaoOAuth == null) {
-				model.addAttribute("__AUTH__", kakaoUserDTO);
-				
-				return "redirect:/sign-up/";
-			}
-			else {
-				model.addAttribute("__AUTH__", kakaoOAuth);
-				
-				return "redirect:/login/callback";
-			}
-		} catch (Exception e) {
-			throw new ControllerException(e);
-		}// try-catch
-
+		return "redirect:/";
 	}// getKakaoCallBack
 
-	@GetMapping("/callback")
-	public String getLoginCallBack(
-			@SessionAttribute(value = "LOGIN_CALL_BACK_URI", required = false)Object loginCallBackURI
-			) {
-		log.trace("getLoginCallBack({}) invoked.", loginCallBackURI);
-
-		if(loginCallBackURI == null) {
-
-			return "redirect:/";
-		} // if
-
-		return "redirect:" + (String) loginCallBackURI;
-
-	} // getLoginCallBack
 
 }// end class
 
---
+++ src/main/java/org/zerock/wego/interceptor/AfterLoginInterceptor.java
@@ -0,0 +1,47 @@
+package org.zerock.wego.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.zerock.wego.config.SessionConfig;
+import org.zerock.wego.domain.UserVO;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+
+@Log4j2
+@NoArgsConstructor
+
+@Component("afterLoginInterceptor")
+public class AfterLoginInterceptor 
+implements HandlerInterceptor{
+
+	@Override
+	public void postHandle(
+			HttpServletRequest req,
+			HttpServletResponse res,
+			Object handler,
+			ModelAndView modelAndView
+			) throws Exception {
+		if(modelAndView != null) {
+			System.err.println(modelAndView);
+
+			UserVO authUserVO = (UserVO) modelAndView.getModelMap().getAttribute(SessionConfig.AUTH_KEY_NAME);
+
+			if(authUserVO != null) {
+				log.trace("postHandle(req,res,handler,modelAndView) invoked.");
+
+				modelAndView.getModelMap().remove(SessionConfig.AUTH_KEY_NAME);
+
+				req.getSession().setAttribute(SessionConfig.AUTH_KEY_NAME,  authUserVO);
+			} // if
+		} // if
+
+	} // postHandle
+
+
+} // end class
