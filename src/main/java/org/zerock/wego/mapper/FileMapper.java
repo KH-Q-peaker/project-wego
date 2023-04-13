5abf0b240e202d81f50cdc38313e1e57ef646ce5
isojin <isojin@218.38.137.28>
2023-04-13 09:17:14 +0900

    <refactor> : @Param추가 (사유: 인식을 못함..)

--- src/main/java/org/zerock/wego/mapper/FileMapper.java
+++ src/main/java/org/zerock/wego/mapper/FileMapper.java
@@ -12,10 +12,10 @@ public interface FileMapper {
 
 	// 타겟의 첨부파일 개수 조회 
 	public abstract Integer selectTotalCountByTarget(@Param("targetGb")String targetGb, @Param("targetCd")Integer targetCd);
-	public abstract List<FileVO> selectByTargetGbAndTargetCd(String targetGb, Integer targetCd); 
+	public abstract List<FileVO> selectByTargetGbAndTargetCd(@Param("targetGb")String targetGb, @Param("targetCd")Integer targetCd); 
 	public abstract Integer insert(FileDTO file); 
 	public abstract Integer update(String targetGb, Integer targetCd, Integer fileId, String fileName);
-  public abstract Integer deleteByTarget(@Param("targetGb")String targetGb, @Param("targetCd")Integer targetCd); // 파일 삭제
+	public abstract Integer deleteByTarget(@Param("targetGb")String targetGb, @Param("targetCd")Integer targetCd); // 파일 삭제
 	public abstract Integer deleteByTargetGbAndTargetCdAndUuid(String targetGb, Integer targetCd, String uuid);
 
 } // end interface
