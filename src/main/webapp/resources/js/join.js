bb103a12144096168634a782f4f2d6d03fd06516
isojin <isojin@218.38.137.28>
2023-04-17 15:10:09 +0900

    어휴 빼먹을뻔

--- src/main/webapp/resources/js/join.js
+++ src/main/webapp/resources/js/join.js
@@ -45,7 +45,7 @@ $(() => { /* 참여하기 모달창 on/off  */
 		$(".joinbtn").off('click').on('click', function(){
 			
 			$.ajax({
-			url : "/join/" + target.targetCd ,
+			url : "/party/join/" + target.targetCd ,
 			type : "POST",
 			success : function(data){
 		 		hideJoinModal();
@@ -74,7 +74,7 @@ $(() => { /* 참여하기 모달창 on/off  */
 			$(".joinbtn").off('click').on('click', function() {
 
 				$.ajax({
-					url : "/join/" + target.targetCd ,
+					url : "/party/join/" + target.targetCd ,
 					type : "POST",
 					success: function(data) {
 						hideJoinModal();
