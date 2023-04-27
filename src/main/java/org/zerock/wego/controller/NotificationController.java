097d10323454dcf0631a88cf3f186ee01781992f
uniyunnnn <kacl2261@gmail.com>
2023-04-27 14:41:54 +0900

    "<refactor> ModelAndView말고 Model로 변경"
    회의 내용을 반영하여 ModelAndView말고 Model로 변경

--- src/main/java/org/zerock/wego/controller/NotificationController.java
+++ src/main/java/org/zerock/wego/controller/NotificationController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.zerock.wego.domain.common.NotificationVO;
 import org.zerock.wego.domain.common.UserVO;
+import org.zerock.wego.exception.ControllerException;
 import org.zerock.wego.service.common.NotificationService;
 import org.zerock.wego.service.common.UserService;
 
@@ -27,8 +29,8 @@ public class NotificationController { // 알림 컨트롤러
 	private final NotificationService service;
 	private final UserService userService;
 	
-    @GetMapping("/{userId}")
-    public ModelAndView showNotificationPage(@PathVariable("userId") Integer userId) {
+	@GetMapping("/{userId}")
+    public String showNotificationPage(@PathVariable("userId") Integer userId, Model model) throws ControllerException {
     	log.trace("showNotificationPage({}) invoked.", userId);
     	
 
@@ -39,13 +41,11 @@ public class NotificationController { // 알림 컨트롤러
                         .distinct()
                         .collect(Collectors.toList())
         ); // 알림생성유저의 개체요소 가져오기
-		ModelAndView mav = new ModelAndView("common/notification");
-		mav.addObject("userId", userId);
-		mav.addObject("notificationList", notificationList);
-		mav.addObject("userService", userService);
-		mav.addObject("createdAlarmUsers", createdAlarmUsers);
-		return mav;
-    } // showNotificationPage
-    
+		model.addAttribute("userId", userId);
+		model.addAttribute("notificationList", notificationList);
+		model.addAttribute("userService", userService);
+		model.addAttribute("createdAlarmUsers", createdAlarmUsers);
+		return "common/notification";
+    } // showNotificationPage      
     
 } // end class 
