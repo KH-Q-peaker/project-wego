5689e949edc34ec7eae2285bf974ea695198bf2e
javahasooingu <ingoo1024@gmail.com>
2023-05-07 21:33:05 +0900

    feat info

--- src/main/java/org/zerock/wego/mapper/SanInfoMapper.java
+++ src/main/java/org/zerock/wego/mapper/SanInfoMapper.java
@@ -3,7 +3,6 @@ package org.zerock.wego.mapper;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.ibatis.annotations.Select;
 import org.zerock.wego.domain.common.BoardDTO;
 import org.zerock.wego.domain.common.BoardSearchDTO;
 import org.zerock.wego.domain.info.SanInfoViewSortVO;
@@ -12,25 +11,17 @@ import org.zerock.wego.domain.info.SanInfoViewVO;
 
 public interface SanInfoMapper {
 
-	@Select("SELECT count(san_info_id) FROM san_info_v WHERE san_info_id > 0")
-	public abstract Integer selectTotalCount();
-	
 	public abstract List<SanInfoViewVO> selectAll();
-	
 	public abstract List<SanInfoViewSortVO> selectAllOrderByAbc(BoardDTO dto);
 	public abstract List<SanInfoViewSortVO> selectAllOrderByLike(BoardDTO dto);
-	
 	public abstract List<SanInfoViewSortVO> selectSearchSanInfoByQueryOrderByAbc(BoardSearchDTO dto);
 //	public abstract List<SanInfoViewSortVO> selectSearchSanInfoByQueryOrderByLike(BoardDTO dto, String query);
 
 	public abstract List<SanInfoViewVO> selectSanInfoSuggestion();
-	
-	
 	public abstract Set<SanInfoViewVO> selectRandom10(); 
 	public abstract SanInfoViewVO selectById(Integer sanInfoId); 
 	public abstract Integer selectIdBySanName(String sanName); 
 	
 	public abstract Set<SanInfoViewVO> selectSearchSanInfo3ByQuery(String query);
-	
 	
 } // end interface
